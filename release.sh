ffe67e565d367c7a09d6d6afbcb2b941b4dd1262
Dorian Podulka <dorian.podulka@satoshilabs.com>
2020-02-09 13:30:11 +0100

    update migrations for heroku

--- release.sh
+++ release.sh
@@ -15,15 +15,18 @@
 #    exit 1
 #fi
 
-echo "Checking migrations..."
-./manage.py makemigrations --check
-exit_code=$?
+#echo "Checking migrations..."
+#./manage.py makemigrations --check
+#exit_code=$?
+#
+#if [ $exit_code -ne 0 ]; then
+#    echo "Models have changes that are not reflected in migrations yet."
+#    echo "Aborting the release."
+#    exit $exit_code
+#fi
 
-if [ $exit_code -ne 0 ]; then
-    echo "Models have changes that are not reflected in migrations yet."
-    echo "Aborting the release."
-    exit $exit_code
-fi
+echo "Making migrations..."
+./manage.py makemigrations || exit $?
 
 echo "Running migrations..."
 ./manage.py migrate || exit $?
