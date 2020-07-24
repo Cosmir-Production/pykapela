f58245a3d204088c2d87c294e3f4571f3147bbb3
Dorian Podulka <dorian.podulka@satoshilabs.com>
2020-07-24 16:08:17 +0200

    add table to tinymce

--- pykapela/settings/tinymce.py
+++ pykapela/settings/tinymce.py
@@ -1,8 +1,8 @@
 
 TINYMCE_DEFAULT_CONFIG = {
     "theme": "silver",
-    "plugins": "code",
-    'menubar': 'edit view format',
+    "plugins": "code,table",
+    'menubar': 'edit view format table',
     'toolbar': 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | code',
     "theme_advanced_buttons3_add": "|",
 }
