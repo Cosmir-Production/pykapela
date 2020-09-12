a8284ab78a36c697b5ad437e4d263a7b8123dd6b
Dorian Podulka <dorian.podulka@satoshilabs.com>
2020-09-12 11:19:20 +0200

    add page translation, upgrade yarn

--- pykapela/pages/admin.py
+++ pykapela/pages/admin.py
@@ -1,4 +1,4 @@
-
+from django.conf import settings
 from django.contrib import admin
 
 from pykapela.pages.models import Page
@@ -7,3 +7,6 @@ from pykapela.pages.models import Page
 @admin.register(Page)
 class PagesAdmin(admin.ModelAdmin):
     list_display = ('title', 'position', 'is_published')
+    fields = tuple('title_' + lang[0] for lang in settings.LANGUAGES) \
+             + tuple('content_' + lang[0] for lang in settings.LANGUAGES) \
+            + ('slug', 'position', 'is_published', 'image', 'portrait_image')
