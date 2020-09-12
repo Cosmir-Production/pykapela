e9fe13cc25555e7d080bfa6930c6f1444676e0a9
Dorian Podulka <dorian.podulka@satoshilabs.com>
2020-09-12 16:22:29 +0200

    hotfix: force concerts url

--- pykapela/urls.py
+++ pykapela/urls.py
@@ -48,6 +48,7 @@ if settings.DEBUG:
 
 urlpatterns += i18n_patterns(
     url(r'concerts', include('pykapela.events.urls')),
+    url(r'concerts/', include('pykapela.events.urls')),
 
 
     url(r'^$', views.WebView.as_view(), name='index'),
