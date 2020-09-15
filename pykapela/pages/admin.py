4b8d78c8283e053e67ea9486cd2e1992597f5e7f
Dorian Podulka <dorian.podulka@satoshilabs.com>
2020-09-15 12:22:47 +0200

    allow edits of is_dark on pages

--- pykapela/pages/admin.py
+++ pykapela/pages/admin.py
@@ -6,7 +6,7 @@ from pykapela.pages.models import Page
 
 @admin.register(Page)
 class PagesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'position', 'is_published')
+    list_display = ('title', 'position', 'is_published', 'is_dark')
     fields = tuple('title_' + lang[0] for lang in settings.LANGUAGES) \
              + tuple('content_' + lang[0] for lang in settings.LANGUAGES) \
-            + ('slug', 'position', 'is_published', 'image', 'portrait_image')
+            + ('slug', 'position', 'is_published', 'is_dark', 'image', 'portrait_image')
