0695d890c05004f9dbc78740856a2c25adb251d4
Dorian Podulka <dorian.podulka@satoshilabs.com>
2025-12-04 18:28:11 +0100

    fix youtube iframe

--- pykapela/settings/tinymce.py
+++ pykapela/settings/tinymce.py
@@ -5,9 +5,9 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': 'edit view format table',
     'toolbar': 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | code',
     "theme_advanced_buttons3_add": "|",
+    "extended_valid_elements": "iframe[src|frameborder|allowfullscreen|referrerpolicy|allow|width|height]",
 }
 
 TINYMCE_SPELLCHECKER = False
 TINYMCE_COMPRESSOR = True
 TINYMCE_INCLUDE_JQUERY = True
-
